80cf1b04ce927435ea480746ca63b6e8f18ea097
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-05-12 16:41:22 +0200

    Updated

---
+++ Base/Sachssoft.Sasospector/Constraints/StringConstraint.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Sachssoft.Sasospector.Constraints
+{
+    public class StringConstraint : InspectorConstraintBase<string?>
+    {
+        public int? MinLength { get; init; }
+        public int? MaxLength { get; init; }
+
+        public bool AllowNullOrWhiteSpace { get; init; } = true;
+
+        public override string? Coerce(string? value)
+        {
+            if (value == null)
+                return value;
+
+            if (MaxLength.HasValue && value.Length > MaxLength.Value)
+                return value[..MaxLength.Value];
+
+            return value;
+        }
+
+        public override ValidationResult Validate(string? value)
+        {
+            if (value is null)
+            {
+                return AllowNullOrWhiteSpace
+                    ? ValidationResult.Success()
+                    : ValidationResult.Fail(
+                        new ArgumentException("Value cannot be null"));
+            }
+
+            if (!AllowNullOrWhiteSpace && string.IsNullOrWhiteSpace(value))
+            {
+                return ValidationResult.Fail(
+                    new ArgumentException("Value cannot be whitespace"));
+            }
+
+            if (MinLength.HasValue && value.Length < MinLength.Value)
+            {
+                return ValidationResult.Fail(
+                    new ArgumentOutOfRangeException(
+                        $"String too short (min {MinLength.Value})"));
+            }
+
+            if (MaxLength.HasValue && value.Length > MaxLength.Value)
+            {
+                return ValidationResult.Fail(
+                    new ArgumentOutOfRangeException(
+                        $"String too long (max {MaxLength.Value})"));
+            }
+
+            return ValidationResult.Success();
+        }
+    }
+}
