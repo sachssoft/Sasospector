847ed1ad056a16f5a6e8fc44fdfe30c49c6df81b
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-05-12 10:25:24 +0200

    Reflection Feature Concept Added

---
+++ Base/Sachssoft.Sasospector.Shared/Schemas/ReflectionSchemaBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sachssoft.Sasospector.Shared.Schemas
+{
+    public class ReflectionSchemaBuilder
+    {
+        private readonly object _owner;
+
+        public ReflectionSchemaBuilder(object owner)
+        {
+            _owner = owner ?? throw new ArgumentNullException(nameof(owner));
+        }
+
+        public object Owner => _owner;
+    }
+}
