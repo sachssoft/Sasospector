f2b66e9cd5cb119afcaf8af3c7c6f9db5aeec96e
sachssoft <60543974+sachssoft@users.noreply.github.com>
2026-05-18 00:35:43 +0200

    Update

---
+++ Platforms/Avalonia/Sachssoft.Sasospector.Avalonia/Views/ToggleableSectionMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sachssoft.Sasospector.Views
+{
+    public enum ToggleableSectionMode
+    {
+        None = 0,
+        Visible = 1,
+        Enabled = 2,
+    }
+}
--- Platforms/Avalonia/Sachssoft.Sasospector.Avalonia/Views/ToggleableSectionView.cs
+++ Platforms/Avalonia/Sachssoft.Sasospector.Avalonia/Views/ToggleableSectionView.cs
@@ -8,27 +8,58 @@ namespace Sachssoft.Sasospector.Views
         private IInspectorPropertyInfo? _property;
 
         public static readonly StyledProperty<string?> PropertyNameProperty =
-            AvaloniaProperty.Register<PropertyViewItem, string?>(nameof(PropertyName));
+            AvaloniaProperty.Register<ToggleableSectionView, string?>(nameof(PropertyName));
 
         public static readonly DirectProperty<ToggleableSectionView, IInspectorPropertyInfo?> PropertyProperty =
             AvaloniaProperty.RegisterDirect<ToggleableSectionView, IInspectorPropertyInfo?>(
                 nameof(Property),
                 o => o.Property);
 
+        public static readonly StyledProperty<bool> IsCheckedProperty =
+            AvaloniaProperty.Register<ToggleableSectionView, bool>(nameof(IsChecked));
+
+        public static readonly StyledProperty<ToggleableSectionMode> ModeProperty =
+            AvaloniaProperty.Register<ToggleableSectionView, ToggleableSectionMode>(nameof(Mode));
+
         protected override Type StyleKeyOverride { get; } = typeof(ToggleableSectionView);
 
-        // Nur Boolean, Int z.b.0 oder 1, ...
         public string? PropertyName
         {
             get => GetValue(PropertyNameProperty);
             set => SetValue(PropertyNameProperty, value);
         }
 
-        // Nur ReadOnly wichtig für Bindings
         public IInspectorPropertyInfo? Property
         {
             get => _property;
             private set => SetAndRaise(PropertyProperty, ref _property, value);
+        }
+
+        public bool IsChecked
+        {
+            get => GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public ToggleableSectionMode Mode
+        {
+            get => GetValue(ModeProperty);
+            set => SetValue(ModeProperty, value);
+        }
+
+        protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
+        {
+            base.OnPropertyChanged(change);
+
+            if (change.Property == PropertyProperty)
+            {
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            // später: Sync Logic zwischen Property + UI
         }
     }
 }
